b0d6bf4432bf259fbc43a5a81f5cbd0892ebf441
kira <kira@mail.ru>
2022-10-08 22:17:55 +0300

    <secondcommit>

--- TelecomNevaSvyaz/TelecomNevaSvyaz/Views/Windows/Autorization.xaml.cs
+++ TelecomNevaSvyaz/TelecomNevaSvyaz/Views/Windows/Autorization.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using TelecomNevaSvyaz.Context;
 using TelecomNevaSvyaz.Views.Windows;
 
@@ -22,12 +23,27 @@ namespace TelecomNevaSvyaz
     /// </summary>
     public partial class Autorization : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
         public Autorization()
         {
             InitializeComponent();
+            txbPassword.IsEnabled = false;
+            txbCode.IsEnabled = false;
+            LoginBtn.IsEnabled = false;
+            DwnBtn.IsEnabled = false;
+
+            timer.Interval = TimeSpan.FromSeconds(3d);
+            timer.Tick += timer_Tick;
         }
 
-        private void CancelBtn_Click(object sender, RoutedEventArgs e) 
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Время вышло!");
+            DwnBtn.IsEnabled = true;
+            timer.Stop();
+        }
+
+        private void CancelBtn_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
@@ -53,10 +69,77 @@ namespace TelecomNevaSvyaz
                     }
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "ошибка");
             }
+        }
+
+        private void txbLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (Data.tns.Employee.Count(item => item.Login == txbLogin.Text) > 0)
+                {
+                    txbPassword.IsEnabled = true;
+                    txbPassword.Focus();
+                }
+                else
+                {
+                    MessageBox.Show("Такого номера нет");
+                }
+            }
+        }
+
+        WindowCode code = new WindowCode();
+        private void txbPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.Key == Key.Enter)
+            {
+                if (Data.tns.Employee.Count(item => item.Password == txbPassword.Text) > 0)
+                {
+                    code.ShowDialog();
+                    txbCode.IsEnabled = true;
+                    txbCode.Focus();
+                    Window_Loaded(null, null);
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (txbCode.IsFocused == true)
+            {
+                if (code.IsActive == false)
+                {
+                    timer.Start();
+                }
+            }
+        }
+
+        private void DwnBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WindowCode code = new WindowCode();
+            code.ShowDialog();
+            timer.Start();
+        }
+
+        private void txbCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txbCode.Text != "")
+            {
+                if (LoginBtn.IsEnabled != true) LoginBtn.IsEnabled = true;
+                timer.Stop();
+            }
+            else
+            {
+                LoginBtn.IsEnabled = false;
+                timer.Start();
+            }
+
+            if (DwnBtn.IsEnabled != true) DwnBtn.IsEnabled = true;
+            
         }
     }
 }
---
+++ TelecomNevaSvyaz/TelecomNevaSvyaz/Views/Windows/WindowCode.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TelecomNevaSvyaz.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowCode.xaml
+    /// </summary>
+    public partial class WindowCode : Window
+    {
+        public WindowCode()
+        {
+            InitializeComponent();
+            txbWindowCode.Text = GenericPassword();
+        }
+        public string GenericPassword()
+        {
+            // Получаем количество слов и букв за слово.
+            int num_letters = 8;
+            int num_words = 1;
+
+            // Создаем массив букв, которые мы будем использовать.
+            char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdifghijklmnopqrstuvwxyz#$%^&@123456789!".ToCharArray();
+
+            // Создаем генератор случайных чисел.
+            Random rand = new Random();
+
+            // Сделайте слово.
+            string word = "";
+            // Делаем слова.
+            for (int i = 1; i <= num_words; i++)
+            {
+                for (int j = 1; j <= num_letters; j++)
+                {
+                    // Выбор случайного числа от 0 до 25
+                    // для выбора буквы из массива букв.
+                    int letter_num = rand.Next(0, letters.Length - 1);
+
+                    // Добавить письмо.
+                    word += letters[letter_num];
+                }
+            }
+            return word;
+        }
+
+
+    }
+}
